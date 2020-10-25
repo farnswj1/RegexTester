611677455c7b7e232df4465b204dc5a71d3dde10
Justin Farnsworth <farnswj1@tcnj.edu>
2020-10-24 23:18:27 -0400

    converted RegexTester into a class

--- regexTester.py
+++ regexTester.py
@@ -15,13 +15,80 @@ import tkinter as tk
 from re import search
 
 
-# Main function
-def main():
+# RegexTester class
+class RegexTester(object):
+    # Constructor
+    def __init__(self):
+        # Initalize the window
+        self.__window = tk.Tk()
+
+        # Set the window title and dimensions
+        self.__window.title("Regex Tester")
+        self.__window.geometry("600x400")
+
+        # Generate the font and size
+        self.__font = ("Comic Sans MS", 20)
+
+        # Regex label
+        self.__regexLabel = tk.Label(
+            self.__window, 
+            text="Enter your regex:", 
+            font=self.__font
+        )
+        self.__regexLabel.place(relx=0.5, y=25, anchor=tk.CENTER)
+
+        # Regex input field
+        self.__regex = tk.StringVar()
+        self.__regexEntered = tk.Entry(
+            self.__window, 
+            textvariable=self.__regex, 
+            font=self.__font
+        )
+        self.__regexEntered.place(relx=0.5, y=75, width=600, height=40, anchor=tk.CENTER)
+
+        # String label
+        self.__stringLabel = tk.Label(
+            self.__window, 
+            text="Enter your text:", 
+            font=self.__font
+        )
+        self.__stringLabel.place(relx=0.5, y=150, anchor=tk.CENTER)
+
+        # String input field
+        self.__string = tk.StringVar()
+        self.__stringEntered = tk.Entry(
+            self.__window, 
+            textvariable=self.__string, 
+            font=self.__font
+        )
+        self.__stringEntered.place(relx=0.5, y=200, width=600, height=40, anchor=tk.CENTER)
+
+        # Button (triggers regex test)
+        self.__button = tk.Button(
+            self.__window, 
+            text="Test Regex", 
+            command=self.__testRegex, 
+            font=self.__font, 
+            bg="#ffff00"
+        )
+        self.__button.place(relx=0.5, y=300, width=300, anchor=tk.CENTER)
+
+        # Match label
+        self.__matchLabel = tk.Label(
+            self.__window, 
+            font=self.__font
+        )
+        self.__matchLabel.place(relx=0.5, y=375, anchor=tk.CENTER)
+
+        # Run the tkinter loop
+        self.__window.mainloop()
+    
+
     # Check if there's a match between the regex and string
     # Triggered when the user clicks the button on the screen
-    def testRegex():
-        input_regex = regex.get()
-        input_string = string.get()
+    def __testRegex(self):
+        input_regex = self.__regex.get()
+        input_string = self.__string.get()
 
         # Run the test if any of the fields are not empty
         if input_regex or input_string:
@@ -34,52 +101,12 @@ def main():
                 match_bg = "#ff0000" # Red
         else: # Both fields are empty
             result = "Please fill out the fields above."
-            match_bg="SystemButtonFace" # Default background color
+            match_bg = "SystemButtonFace" # Default background color
         
         # Update the match label
-        matchLabel.config(text=result, bg=match_bg)
-    
-    
-    # Initalize the window
-    window = tk.Tk()
-
-    # Set the window title and dimensions
-    window.title("Regex Tester")
-    window.geometry("600x400")
-
-    # Generate the font and size
-    font = ("Comic Sans MS", 20)
-
-    # Regex label
-    regexLabel = tk.Label(window, text="Enter your regex:", font=font)
-    regexLabel.place(relx=0.5, y=25, anchor=tk.CENTER)
-
-    # Regex input field
-    regex = tk.StringVar()
-    regexEntered = tk.Entry(window, textvariable=regex, font=font)
-    regexEntered.place(relx=0.5, y=75, width=600, height=40, anchor=tk.CENTER)
-
-    # String label
-    stringLabel = tk.Label(window, text="Enter your text:", font=font)
-    stringLabel.place(relx=0.5, y=150, anchor=tk.CENTER)
-
-    # String input field
-    string = tk.StringVar()
-    stringEntered = tk.Entry(window, textvariable=string, font=font)
-    stringEntered.place(relx=0.5, y=200, width=600, height=40, anchor=tk.CENTER)
-
-    # Button (triggers regex test)
-    button = tk.Button(window, text="Test Regex", command=testRegex, font=font, bg="#ffff00")
-    button.place(relx=0.5, y=300, width=300, anchor=tk.CENTER)
-
-    # Match label
-    matchLabel = tk.Label(window, font=font)
-    matchLabel.place(relx=0.5, y=375, anchor=tk.CENTER)
-
-    # Run the tkinter loop
-    window.mainloop()
+        self.__matchLabel.config(text=result, bg=match_bg)
 
 
 # Execute the program
 if __name__ == "__main__":
-    main()
+    RegexTester()
